3c574a741f992a1d135633d7c0157c2bd1dfc9e3
Trevor Storey <trevor@photoshelter.com>
2019-12-09 16:12:26 -0700

    [master] custom style for select dropdowns

--- src/QueryRow.js
+++ src/QueryRow.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import downArrow from './down-arrow.svg';
 
 class QueryRow extends React.Component {
   state = {
@@ -48,27 +49,36 @@ class QueryRow extends React.Component {
     return (
       <div className="query-row-container">
         <button className="remove-btn" onClick={this.removeRow}>-</button>
-        <select value={this.state.firstParam} onChange={this.selectFirstParam}>
-          <option value="user-email">User Email</option>
-          <option value="screen-width">Screen Width</option>
-          <option value="screen-height">Screen Height</option>
-          <option value="num-visits"># of Visits</option>
-          <option value="first-name">First Name</option>
-          <option value="last-name">Last Name</option>
-          <option value="page-response-time">Page Response time (ms)</option>
-          <option value="domain">Domain</option>
-          <option value="page-path">Page Path</option>
-        </select>
-        <select value={this.state.secondParam} onChange={this.selectSecondParam}>
-          <option value="equals">equals</option>
-          {this.isStringParam ? <option value="contains">contains</option> : null}
-          {this.isStringParam ? <option value="starts-with">starts with</option> : null}
-          {this.isStringParam ? null : <option value="between">between</option>}
-          {this.isStringParam ? null : <option value="greater-than">greater than</option>}
-          {this.isStringParam ? null : <option value="less-than">less than</option>}
-          <option value="in-list">in list</option>
-        </select>
+        <div className="custom-select">
+          <select value={this.state.firstParam} onChange={this.selectFirstParam}>
+            <option value="user-email">User Email</option>
+            <option value="screen-width">Screen Width</option>
+            <option value="screen-height">Screen Height</option>
+            <option value="num-visits"># of Visits</option>
+            <option value="first-name">First Name</option>
+            <option value="last-name">Last Name</option>
+            <option value="page-response-time">Page Response time (ms)</option>
+            <option value="domain">Domain</option>
+            <option value="page-path">Page Path</option>
+          </select>
+          <img src={downArrow} alt="down-arrow"></img>
+        </div>
+        {this.state.secondParam === 'contains' || this.state.secondParam === 'starts-with' ? null : <div className="is-and-div">is</div>}
+        <div className="custom-select">
+          <select value={this.state.secondParam} onChange={this.selectSecondParam}>
+            <option value="equals">equal to</option>
+            {this.isStringParam ? <option value="contains">contains</option> : null}
+            {this.isStringParam ? <option value="starts-with">starts with</option> : null}
+            {this.isStringParam ? null : <option value="between">between</option>}
+            {this.isStringParam ? null : <option value="greater-than">greater than</option>}
+            {this.isStringParam ? null : <option value="less-than">less than</option>}
+            <option value="in-list">in list</option>
+          </select>
+          <img src={downArrow} alt="down-arrow"></img>
+        </div>
         <input type="text" value={this.state.inputValue} onChange={this.handleInputValueChange}></input>
+        {this.state.secondParam === 'between' ? <div className="is-and-div">and</div> : null}
+        {this.state.secondParam === 'between' ? <input type="text" value={this.state.inputValue} onChange={this.handleInputValueChange}></input> : null}
       </div>
     );
   }
