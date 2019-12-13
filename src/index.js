c3ed6d877e3fb877e67d3d1a3e92972c5cb86cfc
Trevor Storey <trevor@photoshelter.com>
2019-12-12 22:51:20 -0700

    [master] cleaned up some leftovers

--- src/index.js
+++ src/index.js
@@ -1,12 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import PredicateBuilder from './PredicateBuilder';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import PredicateBuilder from "./PredicateBuilder";
 
-ReactDOM.render(<PredicateBuilder />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(<PredicateBuilder />, document.getElementById("root"));
